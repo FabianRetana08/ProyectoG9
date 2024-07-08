35b257d854584a15f61db635db1ca5d3f8f63b78
FabianRetana08 <fretana08@gmail.com>
2024-07-07 23:46:36 -0600

    avance grupo 9

---
+++ src/main/java/com/ProyectoG9/LearningArcadeApplication.java
@@ -0,0 +1,13 @@
+package com.ProyectoG9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningArcadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningArcadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ProyectoG9/LearningArcadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectoG9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningArcadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
